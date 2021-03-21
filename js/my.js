a9aacb47db3206565346e23ea0d2551a044da818
Anton-sanitar <77942505+Anton-sanitar@users.noreply.github.com>
2021-03-21 13:50:29 +0300

    go

--- js/my.js
+++ js/my.js
@@ -1,59 +1,51 @@
 let nameInput = document.querySelector('.profile__name');
-let jobInput = document.querySelector('.profile__job');
-let formElementName = document.querySelector('.form__item_name');
-let formElementJob = document.querySelector('.form__item_job');
-let save = document.querySelector('.modal__save');
+let jobInput = document.querySelector('.profile__job'); 
+let formElementName = document.querySelector('.popup__item_name'); 
+let formElementJob = document.querySelector('.popup__item_job');
+let save = document.querySelector('.popup__save');
+ 
+formElementName.value = nameInput.textContent; 
+formElementJob.value = jobInput.textContent; 
+ 
+function formSubmitHandler (evt) { 
+    evt.preventDefault(); 
+    nameInput.textContent = formElementName.value; 
+	jobInput.textContent = formElementJob.value; 
+} 
+ 
+save.addEventListener('submit', formSubmitHandler); 
+ 
+save.addEventListener('click', function () { 
+	nameInput.textContent = formElementName.value; 
+	jobInput.textContent = formElementJob.value; 
+	popupСlose();
+});    
+ 
+ 
+window.addEventListener('keydown', function(e) { 
+    if (e.keyCode === 13) { 
+      	nameInput.textContent = formElementName.value; 
+		jobInput.textContent = formElementJob.value; 
+		popupСlose();
+    } 
+  });  
+ 
+let popup = document.querySelector('.popup');
+let edit = document.querySelector('.profile__edit'); 
+let close = document.querySelector('.popup__close');
+ 
+edit.addEventListener('click', function () {
+	popupOpen();
+});
 
-formElementName.value = nameInput.textContent;
-formElementJob.value = jobInput.textContent;
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameInput.textContent = formElementName.value;
-	jobInput.textContent = formElementJob.value;
+let popupOpen = function () {
+	popup.classList.add('popup_open');
 }
 
-save.addEventListener('submit', formSubmitHandler);
-
-save.addEventListener('click', function () {
-	nameInput.textContent = formElementName.value;
-	jobInput.textContent = formElementJob.value;
-	modal.style.display = "none";
-});   
-
-
-window.addEventListener('keydown', function(e) {
-    if (e.keyCode === 13) {
-      	nameInput.textContent = formElementName.value;
-		jobInput.textContent = formElementJob.value;
-		modal.style.display = "none";
-    }
-  }); 
-
-let edit = document.querySelector('.profile__edit');
-let modal = document.querySelector('.modal');
-let close = document.querySelector('.modal__close');
-
-edit.addEventListener('click', function () {
-    modal.style.display = "flex";
+close.addEventListener('click', function () { 
+    popupСlose(); 
 });
 
-close.addEventListener('click', function () {
-    modal.style.display = "none";
-});
-
-let like = document.querySelectorAll('.card__like');
-
-let add = function (like) {
-  	like.addEventListener('click', function () {
-		if(this.classList.contains('card__like_activ')){
-        	this.classList.remove('card__like_activ');
-	    }else {
-	        this.classList.add('card__like_activ');
-	    }
-	});
-};
-
-for (let i = 0; i < like.length; i++) {
-	add(like[i]);
+let popupСlose = function () {
+	popup.classList.remove('popup_open');
 }
