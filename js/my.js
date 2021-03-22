2d564d7f70152dac7bd6829a36f5c1ec36eaf42e
Anton-sanitar <77942505+Anton-sanitar@users.noreply.github.com>
2021-03-23 01:57:22 +0300

    fix js

--- js/my.js
+++ js/my.js
@@ -1,51 +1,32 @@
-let nameInput = document.querySelector('.profile__name');
-let jobInput = document.querySelector('.profile__job'); 
-let formElementName = document.querySelector('.popup__item_name'); 
-let formElementJob = document.querySelector('.popup__item_job');
-let save = document.querySelector('.popup__save');
- 
-formElementName.value = nameInput.textContent; 
-formElementJob.value = jobInput.textContent; 
- 
-function formSubmitHandler (evt) { 
-    evt.preventDefault(); 
-    nameInput.textContent = formElementName.value; 
-	jobInput.textContent = formElementJob.value; 
-} 
- 
-save.addEventListener('submit', formSubmitHandler); 
- 
-save.addEventListener('click', function () { 
-	nameInput.textContent = formElementName.value; 
-	jobInput.textContent = formElementJob.value; 
-	popupСlose();
-});    
- 
- 
-window.addEventListener('keydown', function(e) { 
-    if (e.keyCode === 13) { 
-      	nameInput.textContent = formElementName.value; 
-		jobInput.textContent = formElementJob.value; 
-		popupСlose();
-    } 
-  });  
- 
 let popup = document.querySelector('.popup');
+let formElementName = document.querySelector('.popup__input_name'); 
+let formElementJob = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.profile__name');
+let jobInput = document.querySelector('.profile__job');
+let save = document.querySelector('.popup__save');
 let edit = document.querySelector('.profile__edit'); 
 let close = document.querySelector('.popup__close');
- 
-edit.addEventListener('click', function () {
-	popupOpen();
-});
 
-let popupOpen = function () {
+edit.addEventListener('click', function () {
 	popup.classList.add('popup_open');
-}
+	formElementName.value = nameInput.textContent; 
+	formElementJob.value = jobInput.textContent;
+});
 
 close.addEventListener('click', function () { 
-    popupСlose(); 
+	popup.classList.remove('popup_open');
 });
 
-let popupСlose = function () {
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    nameInput.textContent = formElementName.value; 
+	jobInput.textContent = formElementJob.value;
+	if (evt.keyCode === 13) {
+		nameInput.textContent = formElementName.value; 
+		jobInput.textContent = formElementJob.value;
+		popup.classList.remove('popup_open');
+	};
 	popup.classList.remove('popup_open');
-}
+};
+
+save.addEventListener('click', formSubmitHandler);
